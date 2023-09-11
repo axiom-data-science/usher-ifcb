2ebbede75225df90aa94b79bca8360d15db5e2e8
Shane St Savage <shane@axds.co>
2023-09-10 21:41:24 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module axds.co/usher-ifcb
+
+go 1.20
+
+replace axds.co/usher => ../usher
+
+require axds.co/usher v0.0.0-00010101000000-000000000000
+
+require (
+	github.com/alecthomas/kong v0.7.1 // indirect
+	github.com/rjeczalik/notify v0.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"axds.co/usher"
+	"errors"
+	"log"
+	"time"
+)
+
+type IfcbFileMapper struct{}
+
+func (fm *IfcbFileMapper) GetFileDestPath(relSrcFile string, absSrcFile string,
+	baseSrcFile string, mappedRootSrcPath string, mappedRootDestPath string,
+	relToMappedRootSrcFile string) (string, error) {
+	//D20230525T192231_IFCB162.adc
+	if baseSrcFile[0] != 'D' {
+		return "", errors.New("file " + relSrcFile + " does not start with D prefix, ignoring")
+	} else if len(baseSrcFile) < 16 {
+		return "", errors.New("file " + relSrcFile + " has a base filename less than 16 characters, ignoring")
+	}
+
+	fileTime, err := time.Parse("20060102T150405", baseSrcFile[1:16])
+	if err != nil {
+		log.Println(err)
+		return "", errors.New("couldn't parse date from file " + relSrcFile + ", ignoring")
+	}
+	destPath := fileTime.Format("2006/D20060102/") + baseSrcFile
+	return destPath, nil
+}
+
+func main() {
+	usher.Run(map[string]usher.FileMapper{
+		"ifcb": &IfcbFileMapper{},
+	})
+}
